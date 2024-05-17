8dd694dcc2de6d759ca00a66390ada7b244b5d34
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2024-05-17 12:14:09 -0400

    update tmux settings

--- .config/tmux/catppuccin.tmux.zsh
+++ .config/tmux/catppuccin.tmux.zsh
@@ -7,9 +7,11 @@ set -g @catppuccin_window_default_fill "number"
 set -g @catppuccin_window_default_text "#W"
 
 set -g @catppuccin_window_current_fill "number"
-set -g @catppuccin_window_current_text "#{pane_current_path}"
+# set -g @catppuccin_window_current_text "#{pane_current_path}"
+set -g @catppuccin_window_current_text "#[bold]#(echo #{pane_current_path} | sed 's#$HOME#~#g')"
+# set -g @catppuccin_window_current_text "#[underscore]#{?#{==:#{window_panes},1},,+}#[bold]#W#[nobold]:#{=|-24|…;s|$HOME|~|:pane_current_path}"
 
-set -g @catppuccin_status_modules_right "application session cpu user clima"
+set -g @catppuccin_status_modules_right "session application cpu uptime clima"
 set -g @catppuccin_status_left_separator  " "
 set -g @catppuccin_status_right_separator ""
 set -g @catppuccin_status_fill "icon"
