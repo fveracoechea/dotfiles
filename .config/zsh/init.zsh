1e3687d40900958b28904c6b9c98c4af0329227a
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2024-07-15 14:04:45 -0400

    Implement zinit and oh-my-posh

---
+++ .config/zsh/init.zsh
@@ -0,0 +1,60 @@
+PATH="$HOME/bin:$PATH"
+
+export XDG_CONFIG_HOME="$HOME/.config"
+
+# Path to your ZINIT installation 
+export ZINIT_HOME="$XDG_CONFIG_HOME/zsh/zinit"
+# source ZINIT
+source "${ZINIT_HOME}/zinit.zsh"
+
+
+
+# Add in zsh plugins
+zinit light zsh-users/zsh-syntax-highlighting
+zinit light zsh-users/zsh-completions
+zinit light zsh-users/zsh-autosuggestions
+zinit light Aloxaf/fzf-tab
+
+zinit ice depth=1
+zinit light jeffreytse/zsh-vi-mode
+
+# Add in snippet
+
+zinit ice wait lucid 2
+zinit snippet OMZP::command-not-found
+
+# Load completions
+autoload -Uz compinit && compinit
+
+
+# History
+HISTSIZE=5000
+HISTFILE=~/.zsh_history
+SAVEHIST=$HISTSIZE
+HISTDUP=erase
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_ignore_dups
+setopt hist_find_no_dups
+
+# Completion styling
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' menu no
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+
+# Aliases
+alias ls='ls --color'
+alias vim='nvim'
+alias c='clear'
+
+# Set up fzf key bindings and fuzzy completion
+source <(fzf --zsh)
+
+if [ "$TERM_PROGRAM" != "Apple_Terminal" ]; then
+  eval "$(oh-my-posh init zsh --config $XDG_CONFIG_HOME/zsh/oh-my-posh/catppuccin.json)"
+fi
