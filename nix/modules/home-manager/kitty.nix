06c361abe512fcf2441913e053fe1c33402ecf24
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2024-08-28 16:54:58 -0400

    Update settings

--- nix/modules/home-manager/kitty.nix
+++ nix/modules/home-manager/kitty.nix
@@ -9,7 +9,7 @@
       italic_font = "FiraCode Nerd Font Italic";
       bold_italic_font = "FiraCode Nerd Font Bold Italic";
       # fix title bar
-      linux_display_server = "x11";
+      # linux_display_server = "x11";
       window_padding_width = 2;
     };
   };
--- nix/modules/nixos/stylix.nix
+++ nix/modules/nixos/stylix.nix
@@ -15,6 +15,12 @@ in {
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     polarity = "dark";
 
+    cursor = {
+      name = "catppuccin-cursors";
+      package = pkgs.catppuccin-cursors.mochaDark;
+      size = 35;
+    };
+
     fonts = {
       serif = firaSans;
       sansSerif = firaSans;
