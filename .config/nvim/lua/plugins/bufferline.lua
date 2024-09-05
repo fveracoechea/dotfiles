09e7c6af466c42af651a3e671751c46e7e3680b1
asdf <asdf>
2024-09-05 11:02:34 -0400

    Update bufferline

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,21 +1,17 @@
 return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  enabled = false,
   version = "*",
   config = function()
     local bufferline = require "bufferline"
     local catppucin_bufferline = require "catppuccin.groups.integrations.bufferline"
-    local mocha = require("catppuccin.palettes").get_palette "mocha"
 
     bufferline.setup {
       options = {
-        mode = "tabs",
-        -- separator_style = "slant",
         offsets = {
           {
-            filetype = "NvimTree",
-            text = " ",
+            filetype = "Directory",
+            text = " File Explorer ",
             highlight = "FileExplorer",
           },
         },
