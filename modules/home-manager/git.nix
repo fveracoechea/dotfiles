4812c358893e0d4d2e36de4981d45af100bb7ef4
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2024-10-15 10:29:13 -0400

    Update git settings

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -5,10 +5,14 @@
     userName = "Francisco Veracoechea";
     extraConfig = {
       core = {
+        editor = "nvim";
         sshCommand = "ssh -i ~/.ssh/id_github";
       };
       pull = {
         rebase = true;
+      };
+      rebase = {
+        autosquash = true;
       };
     };
   };
