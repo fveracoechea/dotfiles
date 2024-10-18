6c0eacc9cee2db1a7f1fe3d1da101b2e9fdff792
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2024-10-17 22:07:30 -0400

    Update styles

--- modules/home-manager/waybar.nix
+++ modules/home-manager/waybar.nix
@@ -7,7 +7,34 @@
     enable = true;
 
     settings = let
-      icon = size: icon: ''<span size="${size}">${icon}</span>'';
+      rosewater = "#f5e0dc";
+      flamingo = "#f2cdcd";
+      pink = "#f5c2e7";
+      mauve = "#cba6f7";
+      red = "#f38ba8";
+      maroon = "#eba0ac";
+      peach = "#fab387";
+      yellow = "#f9e2af";
+      green = "#a6e3a1";
+      teal = "#94e2d5";
+      sky = "#89dceb";
+      sapphire = "#74c7ec";
+      blue = "#89b4fa";
+      lavender = "#b4befe";
+      text = "#cdd6f4";
+      subtext1 = "#bac2de";
+      subtext0 = "#a6adc8";
+      overlay2 = "#9399b2";
+      overlay1 = "#7f849c";
+      overlay0 = "#6c7086";
+      surface2 = "#585b70";
+      surface1 = "#45475a";
+      surface0 = "#313244";
+      base = "#1e1e2e";
+      mantle = "#181825";
+      crust = "#11111b";
+
+      icon = size: icon: ''<span color="${blue}" size="${size}">${icon}</span>'';
       largeIcon = icon "large";
       xlargeIcon = icon "x-large";
       format = icon: label: ''${largeIcon icon}    ${label}'';
@@ -92,7 +119,7 @@
             "cpu"
             "temperature"
             "memory"
-            "disk"
+            # "disk"
           ];
         };
 
@@ -131,8 +158,9 @@
         };
 
         network = {
+          interval = 30;
           format-disconnected = format "󰖪" "0%";
-          format-ethernet = format "" "100%";
+          format-ethernet = format "" "{bandwidthTotalBits}";
           format-linked = "{ifname} (No IP)";
           format-wifi = format "" "{signalStrength}%";
           tooltip-format = "Connected to {essid} {ifname} via {gwaddr}";
@@ -189,15 +217,15 @@
         modules-center = [
           "clock#time"
           "clock#date"
-          "network"
           "pulseaudio"
           "hyprland/workspaces"
+          "network"
           "group/stats"
-          "tray"
-          "gamemode"
         ];
 
         modules-right = [
+          "tray"
+          "gamemode"
           "group/quick-links"
         ];
       };
