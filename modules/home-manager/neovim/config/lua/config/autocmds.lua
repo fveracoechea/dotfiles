f729d526148e70dc6d48911c4f46a5430b79892c
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2026-06-21 15:49:07 -0400

    Disable mini.completion in snacks picker buffers
    Set vim.b.minicompletion_disable = true for snacks_picker_input and
    snacks_picker_list filetypes so completion suggestions don't appear
    while typing in the explorer or any other snacks picker.

--- modules/home-manager/neovim/config/lua/config/autocmds.lua
+++ modules/home-manager/neovim/config/lua/config/autocmds.lua
@@ -6,3 +6,13 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+--- Disable mini.completion in snacks picker buffers (explorer, finders, etc.)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "snacks_picker_input", "snacks_picker_list" },
+  desc = "Disable mini.completion in snacks picker",
+  group = vim.api.nvim_create_augroup("snacks-picker-disable-completion", { clear = true }),
+  callback = function()
+    vim.b.minicompletion_disable = true
+  end,
+})
